b6adbaf974a46e78000a04c009b52613064007ca
Filip Vannfält <filip.vannfalt@gmail.com>
2023-03-02 16:28:43 +0100

    added map functionality

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet'
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  ngOnInit(): void {
+    this.initMap();
+  }
+  private map: L.Map;
+  private center: L.LatLngExpression = [55.722727014745416, 13.21390131868679]; // Kämnärsrätten
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: this.center,
+      zoom: 12
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 10,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+  }
 
 }
